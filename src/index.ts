9cce71abe8afe8138b01a13db666db321b79b6eb
4Bee <4bee.code@gmail.com>
2024-08-13 17:48:37 +0900

    🚩: ts의 enum 선언 방식과 방법

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,35 @@
-//함수 void, never
+//enum : 특정 값, 공통되는 값을 가지고 있고 싶을 때 enum을 사용하면 된다.enum은 key, value로 두 쌍을 가지고 있다는 특징이 있다.
 
-//void: void는 반환되는 값이 없을 때이다. 즉, 함수가 아무런 값을 반환하지 않음을 나타낸다.
-function sayHello(): void {
-  console.log('hello');
+enum Os {
+  Window = 3,
+  Ios = 10,
+  Android
 }
 
-//never : never는 항상 error를 반환하거나 영원히 끝나지 않는 무한loop일 때 사용한다. 즉, 함수가 절대적으로 반환하지 않는 경우에 사용된다.
-function showError(): never {
-  throw new Error();
+console.log(Os['Ios']); //10
+console.log(Os[10]);  //Ios
+
+//enum을 숫자가 아닌 문자열로 지정했을 경우 단방향으로만 작용한다. 즉, 키(Key)는 항상 문자열로 정의 해야한다. 값(Value)을 문자열로 정의하면 키와 혼동이 되기 때문에 그렇다는 거지?
+enum Os2 {
+  Window = 'win',
+  Ios = 'ios',
+  Android = 'and'
 }
 
-function infLoop(): never {
-  while (true) {
-    //do something...
-  }
-}
+//간접 접근
+//key
+console.log(Os2['Window']);
+console.log(Os2['Ios']);
+//value
+// console.log(Os2['ios']); //-> error
+// console.log(Os2['win']); //-> error key는 항상 문자열로 지정되어 있어서 값(value)가 문자열이면 문제가 발생한다.
+
+//그렇다면 문자열로 정의 되어 있는 값(value)에 접근을 어떻게 할까? 아래와 같이 접근할 수 있다.
+
+//직접 접근
+console.log(Os2.Window);
+//값 할당을 통한 직접 접근 방식
+let myOs: Os2;
+
+myOs = Os2.Window
+console.log(myOs);
