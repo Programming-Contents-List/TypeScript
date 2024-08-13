b80ead8337def69c2e4fd3766fde0792708708c1
4Bee <4bee.code@gmail.com>
2024-08-13 16:35:56 +0900

    🚩: 코드 수정

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 //return 값의 타입을 지정하지 않으면 자동으로 void로 설정을 해준다.
 const add = (a: number, b: number) => {
-  console.log(a, b)
+  const result = a + b;
+  console.log(result);
 }
 
-add(1, 2)
+add(1, 2);
