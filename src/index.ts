1d07f39764fb6ea62ea568e81b0dcb58fde1c1a3
4Bee <4bee.code@gmail.com>
2024-08-15 15:31:01 +0900

    🚩: class의 constructor 개념 및 constructor의 this와 property 개념

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,12 @@
-import { Benz, Car } from "./utils/data.interface";
-
-//클래스 : 객체의 행동과 구조를 정의하기에 '='을 사용한다.
-class Bmw implements Car {
-  color = '';
-  constructor(color: string) {
+class Car {
+  // color: string; //-> 생성자에서 this를 사용하기 위해서는 사용될 변수 프로퍼티를 먼제 선언 해야한다.
+  // 하지만 프로퍼티를 선언하지 않고 사용하는 방법이 public과 readonly를 해당 매개변수에 지정해주면 된다.
+  constructor(readonly color: string) {//public
     this.color = color;
   }
-  wheels = 4;
   start() {
-    console.log('go..');
+    console.log("start");
   }
 }
 
-const b = new Bmw('white');
-console.log(b);
-b.start();
-
-//extends
-//객체 리터럴 : 객체를 리터럴해야하기 때문에 key, value 값으로 정의하게 되고 클래스와 달리 '='를 사용해야 한다.
-const benz: Benz = {
-  color: 'black',
-  wheels: 4,
-  start() {
-    console.log('go...');
-  },
-  door: 5,
-  stop() {
-    console.log('stop...');
-  },
-}
+const bmw = new Car("red");
