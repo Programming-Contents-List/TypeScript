6db5bebe030eb7c7ecf8ac9ffda1f5860602e8ca
4Bee <4bee.code@gmail.com>
2024-08-20 15:10:12 +0900

    🚩: Readonly를 사용하는 방법

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-// Required<T> : required는 Partial과 달리 모든 속성들을 필수로 작성해줘야한다.
+// Readonly<T> : ReadOnly는 읽기 전용으로 된다. 이후 값을 변경하려고 하면 error가 발생한다.
 
 import { User } from "./utils/data.interface"
 
-let admin: Required<User> = {
+let admin: Readonly<User> = {
   id: 1,
   name: "Bob",
   age: 30,
 }
+
+// admin.id = 2;  // error-> readonly로 인한 error 발생
