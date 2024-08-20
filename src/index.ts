6f364f77768848b2d0c3f089786b8599838870e8
4Bee <4bee.code@gmail.com>
2024-08-20 15:04:33 +0900

    🚩: Partial을 사용하는 방법

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-import { User } from "./utils/data.interface";
-// 유니온 타입의 user의 key 값을 가지고와 keyof로 간단하게 작성할 수 있다.
-type UserKey = keyof User;
+// Partial<T>
 
-const uk: UserKey = 'id';
+import { User } from "./utils/data.interface"
+
+let admin: Partial<User> = {
+  id: 1,
+  name: "Bob",
+  // job:"" //-> error interface에 없는 객체를 사용하려고 해서 발생하는 에러이다.
+}
