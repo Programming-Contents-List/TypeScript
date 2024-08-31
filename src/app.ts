50f91ac5b40eef378b57bc9a7e3be1adf6d50073
4Bee <4bee.code@gmail.com>
2024-08-31 16:08:19 +0900

    🐛: object로 타입을 지정하면 일반적인 객체로 접근할 수 없다는 error가 발생한다.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-const person = {
-  name: 'Maximilian', //일반적인 객체는 `,`와 같이 사용하지만 '타입스크립트가 추론한 객체 타입'은 `;`와 같이 사용된다.
-  // 즉, 객체는 키와 값으로 한 쌍을 만들지만 추론 객체는 키와 타입으로 쌍을 이룬다.
+const person: object = {
+  name: 'Maximilian',
   age: 30
 };
 
-console.log(person);
+console.log(person.name); //error : Object에 name이 없습니다. 그렇다면 어떻게 해야할까?
