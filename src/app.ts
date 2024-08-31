5469f89737d0e28f7fd61131123b334fefccffa9
4Bee <4bee.code@gmail.com>
2024-08-31 16:06:24 +0900

    🚩: 객체와 추론 객체 타입의 차이

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,7 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2; //result는 number라고 추론을 한다.
-  if (showResult) {
-    console.log(result + phrase);
-  } else {
-    return n1 + n2;
-  }
-}
+const person = {
+  name: 'Maximilian', //일반적인 객체는 `,`와 같이 사용하지만 '타입스크립트가 추론한 객체 타입'은 `;`와 같이 사용된다.
+  // 즉, 객체는 키와 값으로 한 쌍을 만들지만 추론 객체는 키와 타입으로 쌍을 이룬다.
+  age: 30
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-// resultPhrase = 0;  //error -> 타입 추론으로 resultPhrase는 string이다. 따라 0이라는 number타입을 할당할 수 없다.
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person);
