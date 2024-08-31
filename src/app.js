217ca7bbc2ebd821718131ba892832754d2c444b
4Bee <109135643+4BFC@users.noreply.github.com>
2024-08-31 16:46:00 +0900

    🔀: Udemy/Ts/section2/16 (#20)
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱: issue templates 생성
    
    Github에서 작성한 커밋입니다.
    
    * 🌱: pr-template 생성
    
    Github에서 작성한 커밋입니다.
    
    * 🌱: pr-template 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱: pr-template 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🚩: udemy section2의 14번 강의를 듣고 실습을 했다.
    
    * 🚩: 객체와 추론 객체 타입의 차이
    
    * 🐛: object로 타입을 지정하면 일반적인 객체로 접근할 수 없다는 error가 발생한다.
    
    * 🚩: 재활용을 가능한 객체 타입 정의 방법과 객체 정의와의 차이

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
-function add(n1, n2, showResult, phrase) {
-    if (showResult) {
-        console.log(n1 + n2);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+//console.log(person.name); 으로 person.name, 객체 타입으로 접근하기 위한 object 타입 정의 방법
+var person = {
+    name: 'Maximilian',
+    age: 30
+};
+console.log(person.name); //error : Object에 name이 없습니다. 그렇다면 어떻게 해야할까?
