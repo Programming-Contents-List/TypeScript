18ab1fd64c22b0d00d53e32b96d9285cfbeb7e07
4Bee <4bee.code@gmail.com>
2024-08-31 18:43:53 +0900

    🚩: tuple은 유니온 타입으로 되어 있어서 두가지 타입으로 재할당 할 수 있다.

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,20 @@
 const person = {
   name: 'Maximilian',
   age: 30,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  //tuple 튜플은 유니온 타입으로 구성되어 있다.
+  role: [2, 'author']
 };
+
+//유니온으로 구성되어 있기 떄문에 number와 string으로 값을 재할당할 수 있다.
+person.role.push('admin');
+person.role[1] = 10;
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
 
 console.log(person.name);
 
-//hobby: 문자열
 for (const hobby of person.hobbies) {
-  //여기서 hobby는 문자열로 취급이 되고 있다.
-  //이유는 타입 추론으로 인해서다. person.hobbies는 문자열 배열이라고 인식하고 있기 때문이다.
   console.log(hobby);
-  //하지만 map으로 접근을 하게 되면 error가 발생한다. 이유는 문자열은 map으로 사용할 수 없다.
-  // console.log(hobby.map(el => console.log(el)));// 만약 사용하고 싶다면 person.hobbies를 다른 변수로 정의하고 map을 사용하면 문제 없이 사용할 수다.
 }
-
-//arr: 문자열 배열
-let hobby = person.hobbies;
-// let arr = person.hobbies;
-let arr = hobby;
-arr.map(el => console.log(el));
