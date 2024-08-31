b4cc88fe84bf8f33f97ccc4531ebd35928957ee6
4Bee <4bee.code@gmail.com>
2024-08-31 18:55:09 +0900

    🚩: 타입추론방식에서 지정해준 방식으로 타입 객체로 할당했을 때 role은 tuple로 인정하지 않고 유니온 타입 배열로 인식한다.

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ const person: {
   name: string,
   age: number,
   hobbies: string[];
-  role: [number, string];
+  role: (number | string)[];
 } = {
   name: 'Maximilian',
   age: 30,
@@ -13,8 +13,8 @@ const person: {
 
 //유니온으로 구성되어 있기 떄문에 number와 string으로 값을 재할당할 수 있다.
 person.role.push('admin');  //push는 튜플에서 허용되는 예외이다. 오류를 잡지 못한다.
-// person.role[1] = 10;  //error -> string을 할당할 수 없다.
-// person.role = [0, 'admin', 'user']; //이러한 추가 방식도 에러가 발생한다.
+person.role[1] = 10;  //error -> string을 할당할 수 없다.
+person.role = [0, 'admin', 'user']; //이러한 추가 방식도 에러가 발생한다.
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
