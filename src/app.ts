2605922616b3e6110ebcb6785c7c5a7855ca4f6b
4Bee <4bee.code@gmail.com>
2024-08-31 18:50:11 +0900

    🐛: tuple을 잘못사용하는 예시

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,9 @@ const person: {
 };
 
 //유니온으로 구성되어 있기 떄문에 number와 string으로 값을 재할당할 수 있다.
-person.role.push('admin');
-person.role[1] = 10;
+person.role.push('admin');  //push는 튜플에서 허용되는 예외이다. 오류를 잡지 못한다.
+// person.role[1] = 10;  //error -> string을 할당할 수 없다.
+// person.role = [0, 'admin', 'user']; //이러한 추가 방식도 에러가 발생한다.
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
