c2f90f657547e9630aab9bca799c2913757cd4aa
4Bee <4bee.code@gmail.com>
2024-08-31 15:29:13 +0900

    🚩: udemy section2의 15강의를 듣고 실습을 했다. 타입 할당과 타입 추론을 학습했다.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2; //result는 number라고 추론을 한다.
   if (showResult) {
-    console.log(n1 + n2);
+    console.log(result + phrase);
   } else {
     return n1 + n2;
   }
@@ -10,5 +11,6 @@ const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
 const resultPhrase = 'Result is: ';
+// resultPhrase = 0;  //error -> 타입 추론으로 resultPhrase는 string이다. 따라 0이라는 number타입을 할당할 수 없다.
 
 add(number1, number2, printResult, resultPhrase);
