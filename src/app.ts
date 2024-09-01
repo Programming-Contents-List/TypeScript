3579dbfbf1d5e60533dd35a9c57580977d466a2d
4Bee <109135643+4BFC@users.noreply.github.com>
2024-09-01 15:12:29 +0900

    🔀: Udemy/Ts/section2/22 (#27)
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱: issue templates 생성
    
    Github에서 작성한 커밋입니다.
    
    * 🌱: pr-template 생성
    
    Github에서 작성한 커밋입니다.
    
    * 🌱: pr-template 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱: pr-template 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🚩: udemy section2의 14번 강의를 듣고 실습을 했다.
    
    * 🚩: 타입 할당은 지금까지 하나만 가능했다. 여러 타입을 교차로 사용하면 발생하는 error
    
    * 🚩: error TS 2365 매개변수의 탕
    
    * 🚩: 연산이 허용되지 않는 타입 예시
    
    * 🚩: Union Type으로 발생한 TS 2365 문제를 if문으로 정확한 타입을 명시, 지정 해줌으로써 해결

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,21 @@
-const person: {
-  name: string,
-  age: number,
-  hobbies: string[];
-  role: (number | string)[];
-} = {
-  name: 'Maximilian',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
-};
-
-person.role.push('admin');
-person.role[1] = 10;
-person.role = [0, 'admin', 'user'];
-
-//any는 어떤 타입이든 허용하는 타입이다. 어떻게 보면 유연하다고 느껴지질지 모르겠지만 타입스크립트의 장점을 전혀 활용하지 못하기에 좋은 방법은 아니다.
-let favoriteActivities: any[];
-favoriteActivities = ['Sports', 1]; //이렇게 배열에 마구잡이로 서로 다른 타입을 넣어도 아무런 에러가 일어나지 않는다.
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby);
+function combine(n1: number | string, n2: number | string) {
+  let result;
+  if (typeof n1 === 'number' && typeof n2 === 'number') {
+    result = n1 + n2;
+  } else if (typeof n1 === 'string' && typeof n2 === 'string') {
+    result = n1 + n2;
+  } else {
+    result = n1.toString() + n2.toString();
+  } // 해결 방법: if문으로 typeof로 각 타입들이 무엇인지 식별한 후 연산을 적용한다. 연산이 적용되지 않는 타입들은 모두 string으로 형변환 후 연산을 시켜주면 된다. 따라서 유니온 타입으로 여러 타입을 유연하게 사용할 수 있지만 이것들을 활용할 때는 정확히 어떤 타입을 받는지에 관한 로직이 보통 함수나 클래스 구조에 많이 필요하다.
+  return result;
 }
+// Ts가 염려하는 error의 근원은 아래와 같다.
+// const b1: boolean = true;
+// const b2: boolean = false;
+// console.log(b1 + b2);
+
+const CombineAges = combine(20, 30);
+console.log(CombineAges);
+
+const CombineNames = combine('Max', 'Anna');
+console.log(CombineNames);
