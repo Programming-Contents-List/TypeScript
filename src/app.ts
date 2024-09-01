5dbc6856353a46461507659f14d50289e3a4c8d1
4Bee <4bee.code@gmail.com>
2024-09-01 14:33:36 +0900

    🚩: error TS 2365 매개변수의 탕

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-function combine(n1: number, n2: number) {
-  const result = n1 + n2;
+function combine(n1: number | string, n2: number | string) {
+  const result = n1 + n2; //  error -> '+' 연산자를 'string | number' 및 'string | number' 형식에 적용할 수 없습니다.ts(2365) 이러한 에러가 발생한다. 
+  //발생 원인 : TS가 타입추론을 하는데 문제가 발생하는 것이다. 각 매개변수가 여러 타입을 지정을 받았는데 연산을 사용할 수 없는 타입이 있을지 모르기 때문이다. 이를 어떻게 해결할 수 있을꺄?
   return result;
 }
 
 const CombineAges = combine(20, 30);
 console.log(CombineAges);
 
-const CombineName = combine('Max', 'Anna'); //  error -> 이렇게 매개변수에 지정한 타입과 맞지 않으면 error를 발생시킨다. 두 가지 타입 모두를 허용하고 싶으면 어떻게 하면 좋을까?
+const CombineName = combine('Max', 'Anna');
