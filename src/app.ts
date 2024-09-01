e357dd040f608044413b78d36ebf39ab3596dc1d
4Bee <4bee.code@gmail.com>
2024-09-01 14:47:31 +0900

    🚩: Union Type으로 발생한 TS 2365 문제를 if문으로 정확한 타입을 명시, 지정 해줌으로써 해결

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
 function combine(n1: number | string, n2: number | string) {
-  const result = n1 + n2; //  error -> '+' 연산자를 'string | number' 및 'string | number' 형식에 적용할 수 없습니다.ts(2365) 이러한 에러가 발생한다. 
-  //발생 원인 : TS가 타입추론을 하는데 문제가 발생하는 것이다. 각 매개변수가 여러 타입을 지정을 받았는데 연산을 사용할 수 없는 타입이 있을지 모르기 때문이다. 이를 어떻게 해결할 수 있을꺄?
+  let result;
+  if (typeof n1 === 'number' && typeof n2 === 'number') {
+    result = n1 + n2;
+  } else if (typeof n1 === 'string' && typeof n2 === 'string') {
+    result = n1 + n2;
+  } else {
+    result = n1.toString() + n2.toString();
+  } // 해결 방법: if문으로 typeof로 각 타입들이 무엇인지 식별한 후 연산을 적용한다. 연산이 적용되지 않는 타입들은 모두 string으로 형변환 후 연산을 시켜주면 된다. 따라서 유니온 타입으로 여러 타입을 유연하게 사용할 수 있지만 이것들을 활용할 때는 정확히 어떤 타입을 받는지에 관한 로직이 보통 함수나 클래스 구조에 많이 필요하다.
   return result;
 }
 // Ts가 염려하는 error의 근원은 아래와 같다.
-const b1: boolean = true;
-const b2: boolean = false;
-console.log(b1 + b2);
+// const b1: boolean = true;
+// const b2: boolean = false;
+// console.log(b1 + b2);
 
 const CombineAges = combine(20, 30);
 console.log(CombineAges);
 
-const CombineName = combine('Max', 'Anna');
-
+const CombineNames = combine('Max', 'Anna');
+console.log(CombineNames);
