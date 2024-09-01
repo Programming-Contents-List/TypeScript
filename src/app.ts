d0975b308dbc9798d86af1c174ee083df2aedf87
4Bee <4bee.code@gmail.com>
2024-09-01 14:28:10 +0900

    🚩: 타입 할당은 지금까지 하나만 가능했다. 여러 타입을 교차로 사용하면 발생하는 error

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,9 @@
-const person: {
-  name: string,
-  age: number,
-  hobbies: string[];
-  role: (number | string)[];
-} = {
-  name: 'Maximilian',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
-};
-
-person.role.push('admin');
-person.role[1] = 10;
-person.role = [0, 'admin', 'user'];
-
-//any는 어떤 타입이든 허용하는 타입이다. 어떻게 보면 유연하다고 느껴지질지 모르겠지만 타입스크립트의 장점을 전혀 활용하지 못하기에 좋은 방법은 아니다.
-let favoriteActivities: any[];
-favoriteActivities = ['Sports', 1]; //이렇게 배열에 마구잡이로 서로 다른 타입을 넣어도 아무런 에러가 일어나지 않는다.
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby);
+function combine(n1: number, n2: number) {
+  const result = n1 + n2;
+  return result;
 }
+
+const CombineAges = combine(20, 30);
+console.log(CombineAges);
+
+const CombineName = combine('Max', 'Anna'); //  error -> 이렇게 매개변수에 지정한 타입과 맞지 않으면 error를 발생시킨다. 두 가지 타입 모두를 허용하고 싶으면 어떻게 하면 좋을까?
--- src/basics.ts
+++ src/basics.ts
@@ -1,4 +1,4 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+function addTemplate(n1: number, n2: number, showResult: boolean, phrase: string) {
   const result = n1 + n2; //result는 number라고 추론을 한다.
   if (showResult) {
     console.log(result + phrase);
@@ -13,4 +13,29 @@ const printResult = true;
 const resultPhrase = 'Result is: ';
 // resultPhrase = 0;  //error -> 타입 추론으로 resultPhrase는 string이다. 따라 0이라는 number타입을 할당할 수 없다.
 
-add(number1, number2, printResult, resultPhrase);
+addTemplate(number1, number2, printResult, resultPhrase);
+
+const person: {
+  name: string,
+  age: number,
+  hobbies: string[];
+  role: (number | string)[];
+} = {
+  name: 'Maximilian',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author']
+};
+
+person.role.push('admin');
+person.role[1] = 10;
+person.role = [0, 'admin', 'user'];
+
+let favoriteActivities: any[];
+favoriteActivities = ['Sports', 1];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
