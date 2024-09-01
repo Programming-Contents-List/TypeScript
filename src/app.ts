de29070e3f9cd780f0a5b90bfbd9fe377c7ed84c
4Bee <4bee.code@gmail.com>
2024-09-01 22:01:15 +0900

    🚩: 콜백함수로 구현하기 예시

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,22 @@ function printResult(num: number): void {
   console.log('Result: ', +num);
 }
 
+//콜백함수
+function addAndHandle(n1: number, n2: number, cd: (num: number) => void) {
+  const result = n1 + n2;
+  cd(result);
+}
+
+const test = addAndHandle(10, 20, (result) => {
+  console.log(result);
+  return result;
+});
+//콜백함수를 사용하면 어떤 장점이 있는가?
+// 함수 안에서 콜백을 전달하면 타입스크립트는 결과가 숫자라는 것을 추론한다. 즉, 해당 함수를 사용할 때 명시적으로 코드를 작성하지 않아도 된다.
+console.log(test);  //하지만 이렇게 log를 찍어서 확인을 해보면 아무것도 없는 undefined를 반환한다. 왜냐면 반환 타입을 void로 지정했기 때문이다.
+
 printResult(add(1, 2));
 
-// let result: Function;  //일반적인 방식으로 함수 타입을 할당한 경우
-let result: (a: number, b: number) => number;  // 화살표기로 함수의 반환 값까지 할당한 경우
+let result: (a: number, b: number) => number;
 result = add;
-//result = printResult; // error ts(2322) -> 'void' 형식은 'number' 형식에 할당할 수 없습니다.
-// 해당 오류는 result의 타입은 number로 반환되는 함수여야만 가능하기 때문이다. 즉, printResult는 void가 반환이기 때문에 오류가 발생한다.
-// result = 0; //값이 재할당 되면서 0으로 값이 출력된다.
-console.log(result(1, 2));  // 그렇다면 result 변수에 함수 타입을 지정하면 되지 않을까?
+console.log(result(1, 2));
