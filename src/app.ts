2be21e5f6358482152d0247200af96d4e3233ff4
4Bee <4bee.code@gmail.com>
2024-09-01 20:06:05 +0900

    🚩: 함수로 타입을 할당한 변수를 활용한 예시

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,10 @@ function add(n1: number, n2: number): number {
 function printResult(num: number): void {
   console.log('Result: ', +num);
 }
-let result;
-result = printResult(add(1, 2));
-result = 0; //값이 재할당 되면서 0으로 값이 출력된다.
-console.log(result);  //그렇다면 result 변수에 함수 타입을 지정하면 되지 않을까?
+
+printResult(add(1, 2));
+
+let result: Function;
+result = add
+// result = 0; //값이 재할당 되면서 0으로 값이 출력된다.
+console.log(result(1, 2));  //그렇다면 result 변수에 함수 타입을 지정하면 되지 않을까?
