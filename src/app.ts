07b66b2c0dc6153ef36ffd00dc795a57ca2d2e99
4Bee <4bee.code@gmail.com>
2024-09-01 18:43:12 +0900

    🚩: 함수 반환 타입을 지정 했을 때 발생하는 error

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,3 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number): string {
+  return n1 + n2; //error ts(2322) 'number' 형식은 'string' 형식에 할당할 수 없습니다.
 } //현재 return에 정의한 n1+n2로 인해서 add함수의 반환 타입은 Number이다. 만약 toSting으로 형변환을 한 `n1.toString() + n2.toString()`일 경우 반환된 값은 string으로 타입이 반환 될 것이다.
