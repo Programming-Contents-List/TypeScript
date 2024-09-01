3f3344d541b0532b78ee5b27e66a4876035d6857
4Bee <4bee.code@gmail.com>
2024-09-01 20:16:13 +0900

    🚩: 반환 타입을 변수에 직접 지정할 수 있는 방식 예시

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,10 @@ function printResult(num: number): void {
 
 printResult(add(1, 2));
 
-let result: Function;
+// let result: Function;  //일반적인 방식으로 함수 타입을 할당한 경우
+let result: (a: number, b: number) => number;  // 화살표기로 함수의 반환 값까지 할당한 경우
 result = add;
-result = printResult; //이렇게 재할당을 할 경우 undefined를 출력한다. 이유는 printResult의 값이 void이기 때문이다.
+//result = printResult; // error ts(2322) -> 'void' 형식은 'number' 형식에 할당할 수 없습니다.
+// 해당 오류는 result의 타입은 number로 반환되는 함수여야만 가능하기 때문이다. 즉, printResult는 void가 반환이기 때문에 오류가 발생한다.
 // result = 0; //값이 재할당 되면서 0으로 값이 출력된다.
-console.log(result(1, 2));  //그렇다면 result 변수에 함수 타입을 지정하면 되지 않을까?
+console.log(result(1, 2));  // 그렇다면 result 변수에 함수 타입을 지정하면 되지 않을까?
