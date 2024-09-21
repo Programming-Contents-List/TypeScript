24ebd7d8f55103f6e840f2db5f259709b32ce7ae
4Bee <4bee.code@gmail.com>
2024-09-21 15:19:49 +0900

    🚩: private 설정으로 employees[N]에 값을 할당할수 없다. error

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 class Department {
   name: string;
-  employees: string[] = [];
+  private employees: string[] = [];
   constructor(n: string) {
     this.name = n;
   }
@@ -23,8 +23,8 @@ const accounting = new Department('Accounting');
 accounting.addEmployee('Max');
 accounting.addEmployee('Manu');
 
-accounting.employees[2] = 'Anna';
+//accounting.employees[2] = 'Anna'; //employee private 설정으로 error 발생
 //위와 같은 방식으로 코드를 작성하고 실행해도 정상적으로 동작을 하지만 복잡한 코드에서 위와 같은 방식은 좋은 방식이 아니다. 따라서 employee를 접근할 수 없게 해야한다. 따라 접근 범위를 제한할 수 있는 private를 사용하면 된다.
-
+// private는 class 내에서만 접근이 가능하고 외부에서는 접근할수 없다.
 accounting.describe();
 accounting.printEmployeeInformation();
