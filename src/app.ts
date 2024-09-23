2fb018e17b0d48f18247b743813e445c58866f82
4Bee <4bee.code@gmail.com>
2024-09-23 16:07:50 +0900

    🚩: 정적인 static을 사용해서 new, 인스턴스를 사용하지 않고 바로 함수 호출 및 정적 변수 사용 예시

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,15 @@ class Department {
   // private id: string;
   // private name: string;
   private employees: string[] = [];
-  // readonly는 프로퍼티를 초기화한 후 수정할 수 없다. 즉, 한번 할당 되면 변경되면 안되는 고유 번호들을 설정할 때 readonly를 사용한다.
+  static fiscalYear = 2020;
+
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n
+  }
+
+  static createEmployee(name: string) {
+    return { name: name };
   }
 
   describe(this: Department) {
@@ -67,6 +72,9 @@ const accounting = new Department('1', 'Accounting');
 const ITaccounting = new ITDepartment('2', ['Max']);
 
 Math.pow(1, 2); //new, 인스턴스를 사용하지 않고 바로 사용할 수 있는 클래스 Math가 있다. 이와 같이 우리의 class도 구현 해볼 것이다. 방법은 static을 사용하는 것이다.
+
+const employees1 = Department.createEmployee('Max');  //정적 메소드 불러오는 방식
+console.log(employees1, Department.fiscalYear);
 
 ITaccounting.addEmployee('Max');
 ITaccounting.addEmployee('Manu');
