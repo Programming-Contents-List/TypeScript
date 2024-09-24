7412ea41f5610c379d494204a7748ff3edd5b195
4Bee <4bee.code@gmail.com>
2024-09-24 22:11:31 +0900

    🚩: 객체로 새성된 Person의 정보다 잘 적용되고 동작하는지 확인 및 실습

--- src/app.ts
+++ src/app.ts
@@ -17,15 +17,15 @@
 // 
 interface GreetAble {
   name: string;
-  age: number;
   greet(phrase: string): void;
 }
 
 class Person implements GreetAble {
   name: string;
-  age: 30;
-  constructor(n: string) {
+  age: number;
+  constructor(n: string, N: number) {
     this.name = n;
+    this.age = N;
   }
   greet(phrase: string): void {
     console.log(phrase + ' ' + this.name);
@@ -34,13 +34,16 @@ class Person implements GreetAble {
 
 let user1: GreetAble;
 
+user1 = new Person('Mike', 26);
 
-user1 = {
-  name: "Mike",
-  age: 26,
-  greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
-  }
-};
+// user1 = {
+//   name: "Mike",
+//   age: 26,
+//   greet(phrase: string) {
+//     console.log(phrase + ' ' + this.name);
+//   }
+// };
 
 user1.greet('Hi there - I am');
+
+console.log(user1);
