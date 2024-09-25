ea3c4bebc5784fd03deebec917781324d4715bba
4Bee <109135643+4BFC@users.noreply.github.com>
2024-09-25 22:58:10 +0900

    🔀: Udemy/Ts/section5/61: this & binding (#37)
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱: issue templates 생성
    
    Github에서 작성한 커밋입니다.
    
    * 🌱: pr-template 생성
    
    Github에서 작성한 커밋입니다.
    
    * 🌱: pr-template 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱: pr-template 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🚩: udemy section2의 14번 강의를 듣고 실습을 했다.
    
    * 🚩: interface를 학습하기 위해 클래스를 학습하고 실습해보았다.
    
    * 🚩: dist의 컴파일된 js를 확인하기 위해서 es5에서 es6로 ts.config 변경
    
    * 🚩: es6에서 es5로 컴파일 target을 변경
    
    * 🚩: this를 사용해서 출력한 class 내 전역 name 프로퍼티 정상 출력
    
    * 🚩: this키워드를 다시 한번 객체로 감싼 복제 함수 할당 후 출력 undefined 결과 확인, this를 사용시 주의 사항
    
    * 🚩: undefined 재확인
    
    * 🚩: 복제한 class의 함수를 사용 가능하게 하기 위한 방법과 에러 임시적인 해결 단, 똑같이 구현 불가능

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,16 @@ var Department = /** @class */ (function () {
     function Department(n) {
         this.name = n;
     }
+
+    Department.prototype.describe = function () {
+        console.log("Department: " + this.name);
+    };
     return Department;
 }());
 var accounting = new Department('Accounting');
-console.log(accounting);
+accounting.describe();
+var accountingCopy = { name: 'newName', describe: accounting.describe };
+//여기서 undefined를 출력하는 이유는 describe가 참조하는 것이 무엇인지 알 수 없기 때문이다.
+//따라서 accountingCopy와 같이 새로운 객체로 재생성을 해서 할당하기 위해서는 class의 describe함수의 매개변수에
+//this를 넣고 타입지정을 Department로 설정한 뒤(즉, 청사진을 참조하게 설정) accountingCopy에 name 프로퍼티를 기입, 할당해줘야 한다.
+accountingCopy.describe();
