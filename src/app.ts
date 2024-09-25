a9004f1a52fee9135423083a0eaa9d7d0d7ace6f
4Bee <109135643+4BFC@users.noreply.github.com>
2024-09-25 23:10:27 +0900

    🔀: Udemy/Ts/section5/67: getter/setter (#43)
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱: issue templates 생성
    
    Github에서 작성한 커밋입니다.
    
    * 🌱: pr-template 생성
    
    Github에서 작성한 커밋입니다.
    
    * 🌱: pr-template 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱: pr-template 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🚩: udemy section2의 14번 강의를 듣고 실습을 했다.
    
    * 🚩: interface를 학습하기 위해 클래스를 학습하고 실습해보았다.
    
    * 🚩: dist의 컴파일된 js를 확인하기 위해서 es5에서 es6로 ts.config 변경
    
    * 🚩: es6에서 es5로 컴파일 target을 변경
    
    * 🚩: this를 사용해서 출력한 class 내 전역 name 프로퍼티 정상 출력
    
    * 🚩: this키워드를 다시 한번 객체로 감싼 복제 함수 할당 후 출력 undefined 결과 확인, this를 사용시 주의 사항
    
    * 🚩: undefined 재확인
    
    * 🚩: 복제한 class의 함수를 사용 가능하게 하기 위한 방법과 에러 임시적인 해결 단, 똑같이 구현 불가능
    
    * 🚩: 배열 프로퍼티에 접근하는 두 가지 방식
    
    * 🚩: private 설정으로 employees[N]에 값을 할당할수 없다. error
    
    * 🚩: 런타임과 컴파일 과정의 private 영향을 ts, js 파일에서 확인
    
    * 🚩: private를 일반적으로 constructor에서 사용하는 방법
    
    * 🚩: constructor 생성자 내부에 따로 선언하지 않고 매개변수로 선언하는 간단한 방법
    
    * 🚩: readonly의 사용방법과 build된 app.js에서 확인할 수 있는 property 키워드 확인 및 실습
    
    * 🚩: 상속을 구현하고 실습해보았다.
    
    * 🚩: getter와 setter 학습 및 실습

--- src/app.ts
+++ src/app.ts
@@ -15,19 +15,43 @@ class Department {
     // this.id = '2';  // readonly이기 때문에 error가 발생한다.
     this.employees.push(employee);
   }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 class ITDepartment extends Department {
   admins: string[];
-  constructor(id: string, public admin: string[]) {
+  constructor(id: string, admins: string[]) {
     super(id, 'IT');
-    this.admins = admin;
+    this.admins = admins;
   }
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('No report found.');
+  }
+
+  set setMostRecentReport(value: string) {
+    if (!value) {
+      throw new Error('Please pass in a valid value!')
+    }
+    this.addReport(value);
+    this.lastReport = value;  // 여기서 lastReport를 업데이트 그래야 lastReport가 비어있지 않기 때문에 정상적으로 동작을 한다.
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, 'Account');
+    //strictPropertyInitialization 활성화로 초기화 해줘야 함.
+    this.lastReport = reports[0] || ""; // 초기값을 할당 (reports가 비어있으면 빈 문자열)
   }
 
   addReport(text: string) {
@@ -38,7 +62,6 @@ class AccountingDepartment extends Department {
     console.log(this.reports);
   }
 }
-
 const accounting = new Department('1', 'Accounting');
 const ITaccounting = new ITDepartment('2', ['Max']);
 
@@ -51,6 +74,10 @@ ITaccounting.printEmployeeInformation();
 
 const NewAccounting = new AccountingDepartment('d2', []);
 
+// console.log(NewAccounting.mostRecentReport);  //report가 추가되지 않아서 Error
+NewAccounting.setMostRecentReport = 'Year End Report';
 NewAccounting.addReport('Something went wrong...');
+
+console.log(NewAccounting.mostRecentReport);  //report가 있어서 문제없이 출력
 
 NewAccounting.printReports();
