02352ff9efe766af2ddf5784a11b66dc94b9623e
4Bee <4bee.code@gmail.com>
2024-09-25 20:02:16 +0900

    🚩: interface를 사용한 함수 타입 정의 방식과 실습

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
+interface Named {
+  readonly name: string;
+}
+
 // interface는 객체의 구조를 정의하는데 사용된다.(각 객체들의 Type 명세서)
 /* type 함수 커스텀 방식 */
 // type AddFn = (a: number, b: number) => number;
-
-// interface Named {
-//   readonly name: string;
-// }
 
 // let add: AddFn;
 
 // add = (n1: number, n2: number) => {
 //   return n1 + n2;
 // }
+
+/* interface 함수 커스텀 방식 */
+// 사실 여기선 type 커스텀 방식을 사용하는 것이 일반적이다.
+
+interface AddFn {  //익명함수
+  (a: number, b: number): number;
+}
 
 interface GreetAble extends Named {
   greet(phrase: string): void;
