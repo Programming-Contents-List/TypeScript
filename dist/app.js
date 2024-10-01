29ea62a29be360f558efb77d6eba9ee7570690ae
4Bee <4bee.code@gmail.com>
2024-10-01 23:36:39 +0900

    🐛: 우리가 의도한 제네릭의 설정으로 인해 발생한 오류 확인

--- dist/app.js
+++ dist/app.js
@@ -2,8 +2,9 @@
 function merge(objA, objB) {
     return Object.assign({}, objA, objB);
 }
-// 여기서 두번째 인자에 일반 `30`을 넣으면 객체로 접근을 할 수 없을 뿐 아무런 에러가 발생하지 않는다. 우리가 의도한건 object임을 준수 해야하는 것이다.
-const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, 30); // error가 발생하지 않는다.
+// 1. 여기서 두번째 인자에 일반 `30`을 넣으면 객체로 접근을 할 수 없을 뿐 아무런 에러가 발생하지 않는다. 우리가 의도한건 object임을 준수 해야하는 것이다.
+// 2. 이를 해결하기 위해서는 제네릭의 선언 방식을 변경해야 한다.
+const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, 30); // 1. error가 발생하지 않는다. 2. extends를 사용함으로 error 발생 확인
 // const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 const mergeObj2 = merge({ name: 'Max' }, { age: 30 });
 console.log(mergeObj.name);
