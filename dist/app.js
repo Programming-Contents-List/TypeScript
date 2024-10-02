76b4d484d5e0e732f9f44887e092c3cceb88d3fc
4Bee <4bee.code@gmail.com>
2024-10-02 16:10:55 +0900

    🚩: 코드 동작원리를 완벼하게 파악하지 못했음 따라, issue로 생성해서 추후에 다시 살표볼 코드

--- dist/app.js
+++ dist/app.js
@@ -7,6 +7,9 @@ class DataStorage {
         this.data.push(item);
     }
     removeItem(item) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
         this.data.splice(this.data.indexOf(item), 1);
     }
     getItems() {
@@ -19,9 +22,11 @@ textStorage.addItem('Manu');
 textStorage.removeItem('Max');
 console.log(textStorage.getItems());
 const numberStorage = new DataStorage();
-const objStorage = new DataStorage();
-objStorage.addItem({ name: 'Max' });
-objStorage.addItem({ name: 'Manu' });
-objStorage.removeItem({ name: 'Max' }); //Max를 지웠지만 console.log로 확인하면 Max가 남아 있다.
-//이유는 removeItem의 로직이 단순하게 1번째를 삭제하게 되어 있다.
-console.log(objStorage.getItems());
+//객체로 조직화 된 것보다 string이나 다른 자료형을 사용하는 것이 보다 나은 선택이다. 따라 Generics의 타입선언에 object를 제외
+// const objStorage = new DataStorage<object>();
+// const maxObj = { name: 'Max' };
+// objStorage.addItem(maxObj);
+// objStorage.addItem({ name: 'Manu' });
+// objStorage.removeItem(maxObj); //Max를 지웠지만 console.log로 확인하면 Max가 남아 있다.
+// //이유는 removeItem의 로직이 단순하게 1번째를 삭제하게 되어 있다.
+// console.log(objStorage.getItems());
