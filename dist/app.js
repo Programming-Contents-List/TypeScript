86069dd5d8b391d68134e75da3c1d773c547dc6f
4Bee <4bee.code@gmail.com>
2024-10-02 19:29:52 +0900

    🚩: normal한 방식으로 타입을 정의 하는 방식

--- dist/app.js
+++ dist/app.js
@@ -1,32 +1,8 @@
 "use strict";
-class DataStorage {
-    constructor() {
-        this.data = [];
-    }
-    addItem(item) {
-        this.data.push(item);
-    }
-    removeItem(item) {
-        if (this.data.indexOf(item) === -1) {
-            return;
-        }
-        this.data.splice(this.data.indexOf(item), 1);
-    }
-    getItems() {
-        return [...this.data];
-    }
+// 제네릭 유틸리티 타입
+function createCourseGoal(title, description, completeUntil) {
+    return {
+        title: title, description: description, completeUntil: completeUntil
+    };
 }
-const textStorage = new DataStorage();
-textStorage.addItem('Max');
-textStorage.addItem('Manu');
-textStorage.removeItem('Max');
-console.log(textStorage.getItems());
-const numberStorage = new DataStorage();
-//객체로 조직화 된 것보다 string이나 다른 자료형을 사용하는 것이 보다 나은 선택이다. 따라 Generics의 타입선언에 object를 제외
-// const objStorage = new DataStorage<object>();
-// const maxObj = { name: 'Max' };
-// objStorage.addItem(maxObj);
-// objStorage.addItem({ name: 'Manu' });
-// objStorage.removeItem(maxObj); //Max를 지웠지만 console.log로 확인하면 Max가 남아 있다.
-// //이유는 removeItem의 로직이 단순하게 1번째를 삭제하게 되어 있다.
-// console.log(objStorage.getItems());
+// Partial(파셜) 타입
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,20 @@
-class DataStorage<T extends string | number | boolean> {
-  private data: T[] = [];
-  addItem(item: T) {
-    this.data.push(item);
-  }
+// 제네릭 유틸리티 타입
 
-  removeItem(item: T) {
-    if (this.data.indexOf(item) === -1) {
-      return;
-    }
-    this.data.splice(this.data.indexOf(item), 1);
-  }
-
-  getItems() {
-    return [...this.data];
-  }
+// Normal 하게 사용하는 방식은 이러하다. 하지만 이런 방식은 여러 불편함이 존재한다.
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
 }
 
-const textStorage = new DataStorage<string>();
-textStorage.addItem('Max');
-textStorage.addItem('Manu');
-textStorage.removeItem('Max');
-console.log(textStorage.getItems());
+function createCourseGoal(
+  title: string,
+  description: string,
+  completeUntil: Date,
+): CourseGoal {
+  return {
+    title: title, description: description, completeUntil: completeUntil
+  };
+}
 
-const numberStorage = new DataStorage<number>();
-
-//객체로 조직화 된 것보다 string이나 다른 자료형을 사용하는 것이 보다 나은 선택이다. 따라 Generics의 타입선언에 object를 제외
-// const objStorage = new DataStorage<object>();
-// const maxObj = { name: 'Max' };
-// objStorage.addItem(maxObj);
-// objStorage.addItem({ name: 'Manu' });
-// objStorage.removeItem(maxObj); //Max를 지웠지만 console.log로 확인하면 Max가 남아 있다.
-// //이유는 removeItem의 로직이 단순하게 1번째를 삭제하게 되어 있다.
-// console.log(objStorage.getItems());
+// Partial(파셜) 타입
