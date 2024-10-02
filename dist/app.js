ad658c366b890dec0c1ba5028556a0f3ae26b729
4Bee <4bee.code@gmail.com>
2024-10-02 16:03:08 +0900

    🚩: class로 구현한 Generics 사용 방법

--- dist/app.js
+++ dist/app.js
@@ -1,25 +1,27 @@
 "use strict";
-// function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
-//   return Object.assign({}, objA, objB);
-// }
-function countAndDescribe(element) {
-    let descriptionText = "Got no Value";
-    if (element.length === 1) {
-        descriptionText = "Got 1 element";
+class DataStorage {
+    constructor() {
+        this.data = [];
     }
-    else if (element.length > 1) {
-        descriptionText = 'Got ' + element.length + ' elements.';
+    addItem(item) {
+        this.data.push(item);
     }
-    return [element, descriptionText];
-    // return ['Max', 'Tom']; //error 발생 : 타입을 명시했기 때문에 그렇다.
+    removeItem(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
 }
-console.log(countAndDescribe('Hi, there!'));
-//객체 key, value에 합당한 ts Generics
-function extractAndConvert(obj, key) {
-    return 'Value: ' + obj[key];
-}
-//  T : { name: 'Max' }, U : 'name'
-const value = extractAndConvert({ name: 'Max', age: 30 }, 'age'); //age를 추가한 에시
-console.log(value);
-//  위의 설명이 다소 복잡하다. 우선 U는 객체의 key, value 형태로 구성되어 있다. 그리고 U는 T에서 key에 해당되는 값을 상속받아 key타입을 지니고 있다. 즉, name 부분을 반환하는 형태이다. return 값의 반환을 보면 object의 해당 key값을 도출하려는 의도이다. 따라서, console.log(value)의 값은 Max가 된다.
-// 이를 조금더 변형해보자면 object에 age를 추가하면 key: U타입에 age를 입력할 수 있게 된다.
+const textStorage = new DataStorage();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+const numberStorage = new DataStorage();
+const objStorage = new DataStorage();
+objStorage.addItem({ name: 'Max' });
+objStorage.addItem({ name: 'Manu' });
+objStorage.removeItem({ name: 'Max' }); //Max를 지웠지만 console.log로 확인하면 Max가 남아 있다.
+//이유는 removeItem의 로직이 단순하게 1번째를 삭제하게 되어 있다.
+console.log(objStorage.getItems());
--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,28 @@
-// function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
-//   return Object.assign({}, objA, objB);
-// }
-
-// const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-
-// const mergeObj2 = merge({ name: 'Max' }, { age: 30 });
-
-// console.log(mergeObj.name);
-
-//interface와 Generics의 조합
-interface Length {
-  length: number;
-}
-
-function countAndDescribe<T extends Length>(element: T): [T, string] {  //반환값은 Generics를 사용했기에 타입추론이되지만 명확하게 명시를 해줘야한다면 현재와같이 작성하면 된다.
-  let descriptionText = "Got no Value";
-  if (element.length === 1) {
-    descriptionText = "Got 1 element";
-  } else if (element.length > 1) {
-    descriptionText = 'Got ' + element.length + ' elements.';
+class DataStorage<T> {
+  private data: T[] = [];
+  addItem(item: T) {
+    this.data.push(item);
   }
-  return [element, descriptionText];
-  // return ['Max', 'Tom']; //error 발생 : 타입을 명시했기 때문에 그렇다.
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
 }
 
-console.log(countAndDescribe('Hi, there!'));
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
 
-//객체 key, value에 합당한 ts Generics
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) {
-  return 'Value: ' + obj[key];
-}
-//  T : { name: 'Max' }, U : 'name'
-const value = extractAndConvert({ name: 'Max', age: 30 }, 'age'); //age를 추가한 에시
-console.log(value);
-
-//  위의 설명이 다소 복잡하다. 우선 U는 객체의 key, value 형태로 구성되어 있다. 그리고 U는 T에서 key에 해당되는 값을 상속받아 key타입을 지니고 있다. 즉, name 부분을 반환하는 형태이다. return 값의 반환을 보면 object의 해당 key값을 도출하려는 의도이다. 따라서, console.log(value)의 값은 Max가 된다.
-
-// 이를 조금더 변형해보자면 object에 age를 추가하면 key: U타입에 age를 입력할 수 있게 된다.
+const numberStorage = new DataStorage<number>();
+const objStorage = new DataStorage<object>();
+objStorage.addItem({ name: 'Max' });
+objStorage.addItem({ name: 'Manu' });
+objStorage.removeItem({ name: 'Max' }); //Max를 지웠지만 console.log로 확인하면 Max가 남아 있다.
+//이유는 removeItem의 로직이 단순하게 1번째를 삭제하게 되어 있다.
+console.log(objStorage.getItems());
