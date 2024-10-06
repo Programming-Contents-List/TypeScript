12a7dfbc989a86f6351c7272d402f860a7215ff4
4Bee <4bee.code@gmail.com>
2024-10-06 21:07:25 +0900

    🐛: 런타임 Props 검증, Parse 적용으로 Error 해결

--- src/shared/components/Button.components.tsx
+++ src/shared/components/Button.components.tsx
@@ -4,7 +4,7 @@ import Z from "zod";
 //interface
 //Zod about props
 // 1. zod로 props 스키마 정의
-const buttonPropsSchema = Z.object({
+const buttonPropsSchema = Z.object({  //4. Error: 런타임 Props 검증, Parse 미사용으로 Error 발생
   EnterColor: Z.string(),
   LeaveColor: Z.string(),
 })
@@ -12,10 +12,24 @@ const buttonPropsSchema = Z.object({
 // 2. TypeScript 타입으로 추출
 type ButtonProps = Z.infer<typeof buttonPropsSchema>;
 
+// 2-1. 바로 타입만 추출하고 스키마 제거
+/** 
+type ButtonProps = z.infer<
+  z.object({
+    EnterColor: z.string(),
+    LeaveColor: z.string(),
+  })
+>;
+*/
 // 3. 컴포넌트에 타입 설정: FC Generics로 적용하지 않고 해당 Props에 Type을 적용
 // FC 사용방법은 PR Link 참고
-const ButtonComponents = ({EnterColor, LeaveColor}:ButtonProps) => {
+const ButtonComponents = (props:ButtonProps) => {
   const [isHover, setHover] = useState(false);
+
+// 4-1. 직접적으로 Props 변수를 생성
+  const { EnterColor, LeaveColor } = props;
+// 4-2. 런타임 Props 검증
+  buttonPropsSchema.parse(props);
 
   return (
     <div>
